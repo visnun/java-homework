48c7267cff170eb4456a5113eba54d7ceae9bed4
Visait Nunaev <v.nunaev@gmail.com>
2021-04-14 15:31:52 +0300

    exams first++

--- src/exams/first/App.java
+++ src/exams/first/App.java
@@ -7,9 +7,6 @@ import java.util.Arrays;
 
 public class App {
     public static void main(String[] args) {
-        Owner owner1 = new Owner("Имя1", "Фамилия1", LocalDate.of(1984, Month.JUNE, 15));
-        Membership membership1 = new Membership("Разовый", owner1);
-        FitnessClub fitnessClub = new FitnessClub();
-        fitnessClub.register(membership1, "Тренажерный зал");
+
     }
 }
